b0a4789e3a786e774e23046024853ad7eca6b044
omosoi4082 <omosoi4082@gmail.com>
2026-04-16 14:24:32 +0900

    testApi

---
+++ src/feeds/feeds.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { FeedsService } from './feeds.service';
+
+@Controller('feeds')
+export class FeedsController {
+  constructor(private readonly feedsService: FeedsService) {}
+  @Get()
+  getFeeds() {
+    return this.feedsService.getFeeds();
+  }
+}
